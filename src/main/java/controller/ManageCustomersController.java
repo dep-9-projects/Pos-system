3ff2d54e61094803be4f991aed5be8624278380d
tharindudarsh <darshanaruh@gmail.com>
2022-08-05 15:40:30 +0530

    Manage Customers

---
+++ src/main/java/controller/ManageCustomersController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ManageCustomersController {
+
+    public Button btnAdd;
+    public Button btnUpdate;
+    public Button btnRemove;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtContactNum;
+
+    public void initialize(){
+
+
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnRemoveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+}
